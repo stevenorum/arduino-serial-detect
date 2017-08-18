17da00e30dc3604359b18454aa05ab8a9ad44a39
Steve Norum <stevenorum@gmail.com>
2017-08-18 09:46:17 -0400

    Need this on another computer.

--- detect.py
+++ detect.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-import serial.tools.list_ports
+from ard_core import *
+import serial
 
-def find_port():
-    ports = list(serial.tools.list_ports.comports())
-    for p in ports:
-        if int(p.pid) == 29987:
-            return p.device
-
-with serial.Serial(find_port(), 9600, timeout=1) as ser:
-    
-    
+with serial.Serial(find_port(), 9600) as ser:
+#    ser.write(1)
+    while True:
+        data = ser.readline()
+        number = int(data.decode("utf-8").strip())
+        print("Received: {}".format(number))
+        if number >= 0:
+            number = (number + 1) % 128
+            print("Writing: {}".format(number))
+            ser.write(bytearray([number]))
